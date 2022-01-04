34c063d72fd512a7a3c8ee2895a76be43c0b888f
jr <jr_chueng@126.com>
2022-01-04 15:34:59 +0800

    doc:第一次提交

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: zhangjiarun
+ * @Date: 2022-01-04 15:31:20
+ * @LastEditors: [you name]
+ * @LastEditTime: 2022-01-04 15:31:21
+ * @Description: 测试git 
+ */
+
+let myFirstWord = '第一次提交';
