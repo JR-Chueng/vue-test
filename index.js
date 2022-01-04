9fe9f232c06990966b3dfa5b19e692fbf1c542c1
jr <jr_chueng@126.com>
2022-01-04 15:59:58 +0800

    第二次提交

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @Author: zhangjiarun
  * @Date: 2022-01-04 15:31:20
  * @LastEditors: [you name]
- * @LastEditTime: 2022-01-04 15:31:21
+ * @LastEditTime: 2022-01-04 15:59:06
  * @Description: 测试git 
  */
 
 let myFirstWord = '第一次提交';
+let secWord = '第二次提交';
