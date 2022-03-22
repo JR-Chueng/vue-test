5567037ce618b224fbf7d92389b8fba0f38377a4
jr <jr_chueng@126.com>
2022-03-22 16:45:02 +0800

    ✨ feat(*): 添加静态文件

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
  * @Author: zhangjiarun
  * @Date: 2022-01-04 15:31:20
  * @LastEditors: [you name]
- * @LastEditTime: 2022-03-16 17:12:50
+ * @LastEditTime: 2022-03-17 14:35:17
  * @Description: 测试git 
  */
 
 let myFirstWord = '第一次提交';
 let secWord = '第二次提交';
 let thirdWord = '第三次提交';
+
+
 
 //定义类
 class Point {
