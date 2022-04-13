f43f2d1ca319fa0791f1fa96fa232ebcdeceed70
jr <jr_chueng@126.com>
2022-04-13 14:35:19 +0800

    feat(feature):
    完成版本1.0的功能

---
+++ feature1.0.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: zhangjiarun
+ * @Date: 2022-04-13 14:29:21
+ * @LastEditors: [you name]
+ * @LastEditTime: 2022-04-13 14:32:05
+ * @Description: 文件头的一些描述
+ */
+module.exports = {
+    name: 'feature',
+    dec: 'feature 分支',
+    addOptions: [
+        '新增菜单功能',
+        '新增布局功能',
+        '新增路由功能',
+        '新增vuex功能'
+    ]
+}
