6c8df6a4bcd354eb995ec8b8d6a77fca2f2abb18
jr <jr_chueng@126.com>
2022-04-18 11:39:05 +0800

    ✨ feat(common): 基础模块升级

--- common.js
+++ common.js
@@ -2,11 +2,19 @@
  * @Author: zhangjiarun
  * @Date: 2022-04-13 14:03:31
  * @LastEditors: [you name]
- * @LastEditTime: 2022-04-13 14:05:17
+ * @LastEditTime: 2022-04-18 11:30:46
  * @Description: 文件头的一些描述
  */
 
 const file = {
     name: '公共js文件',
-    des: '不与分支关联'
+    des: '不与分支关联',
+    changeLogs: [
+        {
+            id: 1,
+            index: 0,
+            dec: '假设发生基础升级，修改大量公共文件',
+            date: '20220418'
+        }
+    ]
 }
