73c38258f05a3ce83e5b8550a78957b101af762a
jr <jr_chueng@126.com>
2022-04-18 13:38:07 +0800

    feat(feature2.0): 完成feature2.0功能

---
+++ feature2.0.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: zhangjiarun
+ * @Date: 2022-04-18 13:36:30
+ * @LastEditors: [you name]
+ * @LastEditTime: 2022-04-18 13:36:30
+ * @Description: 文件头的一些描述
+ */
+module.exports = {
+    name: 'feature2.0',
+    dec: 'feature 2.0分支',
+    addOptions: [
+        '新增页面功能',
+        '新增通讯功能',
+    ],
+    bugs: [
+        'bug4',
+        'bug5',
+        'bug6'
+    ]
+}
